d3fc1f6798c91e421803b703ca968766bb1dc9dc
tadekjezus <tadekjezus@gmail.com>
2020-01-05 15:45:50 +0100

    zadanie 11

--- zadanie 11/zadanie 11/Form1.vb
+++ zadanie 11/zadanie 11/Form1.vb
@@ -1,12 +1,12 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim pln As ULong
+        Dim pln As Decimal
 
         Dim banknoty As New List(Of Integer)({200, 100, 50, 20, 10})
         Dim monety As New List(Of Integer)({5, 2, 1})
 
         If IsNumeric(TextBox1.Text) = True Then
-            pln = CULng(TextBox1.Text)
+            pln = Int(TextBox1.Text)
         Else
             MsgBox("nieprawidłowa wartość pln")
             Exit Sub
@@ -16,14 +16,24 @@
         Dim iloscMonet As New Dictionary(Of Integer, Integer)
 
         For Each banknot In banknoty
-            Dim ilosc = pln / banknot
+            Dim ilosc As Integer = pln \ banknot
             iloscBanknotow.Add(banknot, ilosc)
             pln -= ilosc * banknot
         Next
 
-        'TODO formatowanie
-        '"Banknot: " + ilosBanknotow.Key + " ilość: " + iloscBanknotow[key]
-        MsgBox(iloscBanknotow.ToString())
+        For Each moneta In monety
+            Dim ilosc As Integer = pln \ moneta
+            iloscMonet.Add(moneta, ilosc)
+            pln -= ilosc * moneta
+        Next
+
+        Dim message As String = $"Banknoty:{Environment.NewLine}"
+        message += String.Join(Environment.NewLine, iloscBanknotow.Select(Function(kvp) String.Format("{0}: {1}", kvp.Key, kvp.Value)).ToArray())
+
+        message += $"{Environment.NewLine}Monety:{Environment.NewLine}"
+        message += String.Join(Environment.NewLine, iloscMonet.Select(Function(kvp) String.Format("{0}: {1}", kvp.Key, kvp.Value)).ToArray())
+
+        MsgBox(message)
 
     End Sub
 End Class
